53bde9a7c706607685593a5e88435a65002d87de
Brad Bartlett <bradbartlett1@me.com>
2019-07-03 14:36:14 -0400

    essage

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,18 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def create
+    end
+
 end
