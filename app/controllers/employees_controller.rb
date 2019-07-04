f0f5da1f4cb77b3c94e0c62b9eb849f1235d5c0f
github username <github email address>
2019-07-04 17:06:38 -0400

    some of it made sense

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def update 
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit! #return all the values under employees
+    end
+
+
+
+
 end
